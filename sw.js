f0c66e9aa99109217e4aebfbb04a924f897543f4
yungtsunade <brunonilsschroeder@googlemail.com>
2026-07-18 13:10:32 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'planty-v1';
+const ASSETS = [
+  '/planty/index.html',
+  '/planty/manifest.json',
+  '/planty/icons/icon-192.png',
+  '/planty/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(res => {
+    const clone = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, clone));
+    return res;
+  }).catch(() => caches.match(e.request)));
+});
